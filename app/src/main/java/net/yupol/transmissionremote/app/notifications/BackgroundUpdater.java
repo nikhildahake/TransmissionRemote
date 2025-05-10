03b11e54cdb6428c87699d4fb27e9ef86213cd65
Nikhil Dahake <nikhildahake@hotmail.com>
2025-05-10 17:34:21 +0530

    Use TransmissionRemotePreferenceManager instead of PreferenceManager

--- app/src/main/java/net/yupol/transmissionremote/app/notifications/BackgroundUpdater.java
+++ app/src/main/java/net/yupol/transmissionremote/app/notifications/BackgroundUpdater.java
@@ -1,14 +1,14 @@
 package net.yupol.transmissionremote.app.notifications;
 
 import android.content.Context;
-import android.preference.PreferenceManager;
 
 import net.yupol.transmissionremote.app.R;
+import net.yupol.transmissionremote.app.utils.TransmissionRemotePreferenceManager;
 
 public class BackgroundUpdater {
 
     public static void start(Context context) {
-        boolean onlyUnmeteredNetwork = PreferenceManager.getDefaultSharedPreferences(context)
+        boolean onlyUnmeteredNetwork = TransmissionRemotePreferenceManager.getDefaultSharedPreferences(context)
                 .getBoolean(context.getString(R.string.background_update_only_unmetered_wifi_key), true);
         BackgroundUpdateJob.schedule(onlyUnmeteredNetwork);
     }
